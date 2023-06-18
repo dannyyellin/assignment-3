d9e03929a94aba45fafe3d3b34b62fba2dfc71a6
Danny Yellin <34856327+dannyyellin@users.noreply.github.com>
2023-06-18 08:51:49 +0300

    updates

--- solver.py
+++ solver.py
@@ -1,53 +1,20 @@
 import requests
 import sys
+import json
 from Ninja_key import NINJA_API_KEY
 
 
 def get_food_info(food):
-        api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(food)
-        try:
-            response = requests.get(api_url, headers={'X-Api-Key': NINJA_API_KEY})
-        except:
-            print("Error from api.api-ninjas.com/v1/nutrition.  response code = ", response.status_code)
-            print("response text = ", response.text)
-            print("raising SomeAPIError")
-            sys.stdout.flush()
-            raise Exception #SomeAPIError
-        else:
-            if response.status_code == requests.codes.ok:
-                try:
-                    # the response might include multiple items.  E.g., if the query to Ninja is for "cereal and eggs", it
-                    # will return one item for cereal and one for eggs.  However, "cereal and eggs" is treated as one dish,
-                    # so the calories, sodium, etc. of the dish is the sum of the calories, sodium, etc. of the items in the
-                    # response.
-                    resp = response.json()
-                    if not resp:  # response.json() is empty
-                        raise Exception
-                    calories = 0
-                    serving_size = 0
-                    sodium = 0
-                    sugar = 0
-                    for val in resp:
-                        calories += val.get("calories")
-                        sodium += val.get("sodium_mg")
-                        sugar += val.get("sugar_g")
-                    return calories, sodium, sugar
-                except:  # if API returns empty text, then this dish is not found.   Raise DishNotDefined exception
-                    print("Response Error from api.api-ninjas.com/v1/nutrition.  response code = ",
-                          response.status_code)
-                    print("response text = ", response.text)
-                    # print("if empty text then API did not recognize dish called ", name)
-                    print("raising DishNotDefined")
-                    sys.stdout.flush()
-                    raise Exception #DishNotDefined
-            else:  # bad response code from api.api-ninjas.com/v1/nutrition.   Probably "502" Internal Server error.  Need
-                # to retry
-                print("Bad response code from api.api-ninjas.com/v1/nutrition.  response code = ", response.status_code)
-                print("response text = ", response.text)
-                print("raising APINotReachable")
-                sys.stdout.flush()
-                raise Exception # APINotReachable  # need to add custom exception class to handle the different exceptions
-
+    URL = "http://127.0.0.1:8000/dishes"
+    item =  {"name": food}
+    response = requests.post(url=URL, headers={"Content-Type": "application/json"}, data=json.dumps(item))
+    ID = requests.json()
+    response2 = requests.get(f'{URL}/{ID}', headers={"Content-Type": "application/json"})
+    val = response2.json()
+    calories = val.get("calories")
+    sodium = val.get("sodium_mg")
+    sugar = val.get("sugar_g")
+    return calories, sodium, sugar
 
 if __name__ == '__main__':
     fh_query = open("query.txt", 'r')
@@ -61,5 +28,3 @@ if __name__ == '__main__':
             '{:1f}'.format(sug) + " grams of sugar\n"
         fh_resp.write(s)
     fh_resp.close()
-
-
